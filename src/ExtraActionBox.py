410d3d76ae2d8184dbcec3f65b231c518983eb70
nautilus7 <spyros.n7@gmail.com>
2019-10-30 17:08:54 +0200

    Cleanup to class "ExtraActionBox"
    Removed the useless, nasty icon

--- src/ExtraActionBox.py
+++ src/ExtraActionBox.py
@@ -7,26 +7,21 @@ from Components.Pixmap import Pixmap
 class ExtraActionBox(Screen):
 
 	skin = """
-		<screen name="ExtraActionBox" position="center,center" size="560,70" title=" ">
-			<widget alphatest="on" name="logo" position="10,10" size="48,48" transparent="1" zPosition="2"/>
-			<widget font="Regular;20" halign="center" name="message" position="10,10" size="538,48" valign="center" />
+		<screen name="ExtraActionBox" position="center,center" size="560,70">
+			<widget name="message" position="10,10" size="538,48" font="Regular;20" halign="center" valign="center" />
 		</screen>"""
 
 	def __init__(self, session, message, title, action):
 		Screen.__init__(self, session)
-		self.session = session
 		self.ctitle = title
 		self.caction = action
 
 		self["message"] = Label(message)
-		self["logo"] = Pixmap()
 		self.timer = eTimer()
 		self.timer.callback.append(self.__setTitle)
 		self.timer.start(500, 1)
 
 	def __setTitle(self):
-		if self["logo"].instance is not None:
-			self["logo"].instance.setPixmapFromFile(resolveFilename(SCOPE_CURRENT_PLUGIN, 'SystemPlugins/DeviceManager/icons/run.png'))
 		self.setTitle(self.ctitle)
 		self.timer = eTimer()
 		self.timer.callback.append(self.__start)
